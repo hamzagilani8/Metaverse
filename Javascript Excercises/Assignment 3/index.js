f73b28251bbaa313c518a69b94aeb556380871eb
Syed Hamza Gilani <hamzagilani8@gmail.com>
2022-10-09 17:54:39 +0500

    Assignment 3 & 4

--- Javascript Excercises/Assignment 3/index.js
+++ Javascript Excercises/Assignment 3/index.js
@@ -1,3 +1,3 @@
-var famousPerson = "Danial Nagori"
-var qoute = "Remove one word from your life that is TOMORROW. Whatever you have to do, do it on the spot and with CONSISTENCY";
-document.getElementById("qoute").innerHTML = ' ' + famousPerson + ' once said ' + '" ' + qoute + ' "';
+
+var qoute = 'Daniyal Nagori once said "Remove one word from your life that is TOMORROW. Whatever you have to do, do it on the spot and with CONSISTENCY"';
+document.getElementById("qoute").innerHTML = qoute;
---
+++ Javascript Excercises/Assignment 4/index.js
@@ -0,0 +1,3 @@
+var famous_person = "Danial Nagori"
+var qoute = "Remove one word from your life that is TOMORROW. Whatever you have to do, do it on the spot and with CONSISTENCY";
+document.getElementById("qoute").innerHTML = ' ' + famous_person + ' once said ' + '" ' + qoute + ' "';
