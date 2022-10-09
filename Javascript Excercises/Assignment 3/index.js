df9e1a8d572c69d64fcffc543531785ff224455f
Syed Hamza Gilani <hamzagilani8@gmail.com>
2022-10-09 17:50:51 +0500

    Assignment 3

---
+++ Javascript Excercises/Assignment 3/index.js
@@ -0,0 +1,3 @@
+var famousPerson = "Danial Nagori"
+var qoute = "Remove one word from your life that is TOMORROW. Whatever you have to do, do it on the spot and with CONSISTENCY";
+document.getElementById("qoute").innerHTML = ' ' + famousPerson + ' once said ' + '" ' + qoute + ' "';
